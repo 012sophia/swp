896bfb496b90bff8eb7cc63f7ac079fe5202bd83
Sophia Hammermüller <HAM240106@spengergasse.at>
2025-10-03 11:37:05 +0200

    HÜ 29.9

---
+++ Brueche_addieren/2025-09-29_bruch/bruch_test.ts
@@ -0,0 +1,41 @@
+import { assertEquals , assertThrows } from "@std/assert";
+import { Bruch } from "./bruch.ts";
+
+// Testfall 1: 1/2 + 1/3 = 5/6
+Deno.test("bruchTest", () => {
+  const bruch1 = Bruch.fromString("1/2");
+  const bruch2 = Bruch.fromString("1/3");
+  assertEquals(bruch1.addiere(bruch2).toString(), "5/6");
+});
+
+// Testfall 2: 1 1/4 + 2 3/4 = 4
+Deno.test("ganze und gemischte Zahlen", () => {
+  const bruch1 = Bruch.fromString("1 1/4");
+  const bruch2 = Bruch.fromString("2 3/4");
+  assertEquals(bruch1.addiere(bruch2).toString(), "4");
+});
+
+// Testfall 3: Ungültiges Format → Fehler
+Deno.test("ungültiger String", () => {
+  assertThrows(
+    () => Bruch.fromString("abc"),
+    Error,
+    "Ungültige Zahlen im Eingabestring."
+  );
+});
+
+// Testfall 4: Nenner ist 0 → Fehler
+Deno.test("division durch null", () => {
+  assertThrows(
+    () => Bruch.fromString("3/0"),
+    Error,
+    "Nenner darf nicht 0 sein."
+  );
+});
+
+// Testfall 5: Ganze Zahlen addieren
+Deno.test("ganze Zahlen addieren", () => {
+  const bruch1 = Bruch.fromString("2");
+  const bruch2 = Bruch.fromString("3");
+  assertEquals(bruch1.addiere(bruch2).toString(), "5");
+});
